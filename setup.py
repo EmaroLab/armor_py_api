fc6122e215a04fbd6e47d989c4365ace19b7cd8b
Alessio Capitanelli <alessio.capitanelli@gmail.com>
2016-05-25 13:54:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     scripts=['scripts/pyarmor.py'],
-    packages=['pyarmor_client_lib'],
+    packages=['pyarmor'],
     package_dir={'': 'scripts'},
 )
 
